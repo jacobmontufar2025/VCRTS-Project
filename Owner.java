835a93b3512ed23c859936fde5b48d6a9a19e582
Lior4224 <polischouklior@gmail.com>
2025-10-04 19:05:38 -0400

    added 'Log out' button to Owner and Client panels

--- Owner.java
+++ Owner.java
@@ -9,17 +9,39 @@ public class Owner extends JFrame {
       setDefaultCloseOperation(EXIT_ON_CLOSE);
       setLocationRelativeTo(null);
       
-      
-      JLabel label = new JLabel("Welcome, " + OwnerName + "!", SwingConstants.CENTER);
+      // Main panel
       JPanel mainPanel = new JPanel();
       mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
       mainPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));// padding top,left,bottom, right
-
       mainPanel.setBackground(new Color(187, 213, 237));
       add(mainPanel);
+
+      // Welcome message
+      JLabel label = new JLabel("Welcome, " + OwnerName + "!", SwingConstants.CENTER);
+      label.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+      // Back button panel
+      JPanel boxPanel = new JPanel();
+      boxPanel.setLayout(new BoxLayout(boxPanel, BoxLayout.X_AXIS));
+      boxPanel.setOpaque(false);
+      boxPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+      // Back button
+      JButton backButton = new JButton("Log out");
+      backButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+      backButton.addActionListener(e -> {
+          dispose(); 
+          new LandingPage(); 
+      });
+      boxPanel.add(backButton);
+
+      // Add fields to mainPanel
+      mainPanel.add(boxPanel);
+      mainPanel.add(Box.createVerticalStrut(20)); 
       mainPanel.add(label);
+
       setVisible(true);
-  }
+    }
   
   public void rentCar() {
       System.out.println("Car rented!");
